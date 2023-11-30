7cdd065e0ccdf368b3450ff65c4f683798d8b063
Carlos Henrique dos Santos <71515378+carloshsanto@users.noreply.github.com>
2023-11-30 14:27:26 -0300

    Criação projeto next js

---
+++ Front End NextJS/blog/app/layout.js
@@ -0,0 +1,18 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'Blogzila ',
+  description: 'Informações de Tecnologia',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-br">
+      <body>
+        <div>
+          
+        </div>
+      </body>
+    </html>
+  )
+}
