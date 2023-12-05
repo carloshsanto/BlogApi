7059494fa972cc3eee8f739f4d655144b813eb76
Carlos Henrique dos Santos <71515378+carloshsanto@users.noreply.github.com>
2023-12-05 15:45:21 -0300

    Update page.js

--- Front End NextJS/blog/app/page.js
+++ Front End NextJS/blog/app/page.js
@@ -2,10 +2,8 @@ export default function Home() {
   return (
     <>
       <header className="navbar-light navbar-sticky header-static border-bottom navbar-dashboard">
-        {/* Logo Nav START */}
         <nav className="navbar navbar-expand-xl">
           <div className="container">
-            {/* Logo START */}
             <a className="navbar-brand me-3">
               <img
                 className="navbar-brand-item light-mode-item"
@@ -18,22 +16,26 @@ export default function Home() {
                 alt="logo"
               />
             </a>
-            {/* Logo END */}
-            {/* Responsive navbar toggler */}
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-            {/* Main navbar START */}
+
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
             <div className="collapse navbar-collapse" id="navbarCollapse">
               <ul className="navbar-nav navbar-nav-scroll mx-auto">
-                {/* Nav item 1 Demos */}
                 <li className="nav-item">
                   <a className="nav-link" href="dashboard.html">
                     <i className="bi bi-house-door me-1" />
                     Dashboard
                   </a>
                 </li>
-                {/* Nav item 2 Post */}
                 <li className="nav-item dropdown">
                   <a
                     className="nav-link dropdown-toggle"
@@ -47,7 +49,6 @@ export default function Home() {
                     Post
                   </a>
                   <ul className="dropdown-menu" aria-labelledby="postMenu">
-                    {/* dropdown submenu */}
                     <li>
                       {" "}
                       <a
@@ -86,87 +87,172 @@ export default function Home() {
                     </li>
                   </ul>
                 </li>
-                {/* Nav item 3 Pages */}
+
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    id="pagesMenu"
+                    data-bs-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    <i className="bi bi-folder me-1" />
+                    Usuarios
+                  </a>
+                  <ul className="dropdown-menu" aria-labelledby="pagesMenu">
+                    <li>
+                      {" "}
+                      <a
+                        className="dropdown-item"
+                        href="dashboard-author-list.html"
+                      >
+                        Author List
+                      </a>
+                    </li>
+                    <li>
+                      {" "}
+                      <a
+                        className="dropdown-item"
+                        href="dashboard-author-single.html"
+                      >
+                        Author Single
+                      </a>
+                    </li>
+                    <li>
+                      {" "}
+                      <a
+                        className="dropdown-item"
+                        href="dashboard-edit-profile.html"
+                      >
+                        Edit Profile
+                      </a>
+                    </li>
+                    <li>
+                      {" "}
+                      <a
+                        className="dropdown-item"
+                        href="dashboard-reviews.html"
+                      >
+                        Reviews
+                      </a>
+                    </li>
+                    <li>
+                      {" "}
+                      <a
+                        className="dropdown-item"
+                        href="dashboard-settings.html"
+                      >
+                        Settings
+                      </a>
+                    </li>
+                    <li className="dropdown-divider" />
+                    <li>
+                      {" "}
+                      <a
+                        className="dropdown-item"
+                        href="https://support.webestica.com/"
+                        target="_blank"
+                      >
+                        {" "}
+                        <i className="text-warning fa-fw bi bi-life-preserver me-2" />
+                        Support
+                      </a>
+                    </li>
+                    <li>
+                      {" "}
+                      <a
+                        className="dropdown-item"
+                        href="docs/index.html"
+                        target="_blank"
+                      >
+                        {" "}
+                        <i className="text-danger fa-fw bi bi-card-text me-2" />
+                        Documentation
+                      </a>
+                    </li>
+                    <li className="dropdown-divider" />
+                    <li>
+                      <a
+                        className="dropdown-item"
+                        href="rtl/index.html"
+                        target="_blank"
+                      >
+                        {" "}
+                        <i className="text-info fa-fw bi bi-toggle-off me-2" />
+                        RTL demo
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="dropdown-item"
+                        href="https://themes.getbootstrap.com/store/webestica/"
+                        target="_blank"
+                      >
+                        {" "}
+                        <i className="text-success fa-fw bi bi-cloud-download-fill me-2" />
+                        Buy blogzine!
+                      </a>{" "}
+                    </li>
+                  </ul>
+                </li>
+
                 <li className="nav-item dropdown align-items-center">
-                <a
-                  className="avatar avatar-sm p-0 show"
-                  href="#"
-                  id="profileDropdown"
-                  role="button"
-                  data-bs-auto-close="outside"
-                  data-bs-display="static"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="true"
-                >
-                  <img
-                    className="avatar-img rounded-circle"
-                    src="assets/images/avatar/03.jpg"
-                    alt="avatar"
-                  />
-                </a>
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    id="postMenu"
+                    data-bs-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    <i className="bi bi-person-circle me-1" />
+                    Louis Ferguson
+                  </a>
                   <ul
-                  className="dropdown-menu dropdown-animation dropdown-menu-end shadow pt-3 show"
-                  aria-labelledby="profileDropdown"
-                  data-bs-popper="static"
-                >
-                  {/* Profile info */}
-                  <li className="px-3">
-                    <div className="d-flex align-items-center">
-                      {/* Avatar */}
-                      <div className="avatar me-3">
-                        <img
-                          className="avatar-img rounded-circle shadow"
-                          src="assets/images/avatar/03.jpg"
-                          alt="avatar"
-                        />
+                    className="dropdown-menu dropdown-animation dropdown-menu-end shadow pt-3 show"
+                    aria-labelledby="profileDropdown"
+                    data-bs-popper="static"
+                  >
+                    <li className="px-3">
+                      <div className="d-flex align-items-center">
+                        <div className="avatar me-3">
+                          <img
+                            className="avatar-img rounded-circle shadow"
+                            src="assets/images/avatar/03.jpg"
+                            alt="avatar"
+                          />
+                        </div>
+                        <div>
+                          <p className="small m-0">example@gmail.com</p>
+                        </div>
                       </div>
-                      <div>
-                        <a className="h6 mt-2 mt-sm-0" href="#">
-                          {" "}
-                          Louis Ferguson
-                        </a>
-                        <p className="small m-0">example@gmail.com</p>
+                      <hr />
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        <i className="bi bi-person fa-fw me-2" />
+                        Edit Profile
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        <i className="bi bi-gear fa-fw me-2" />
+                        Account Settings
+                      </a>
+                    </li>
+                    <li className="dropdown-divider mb-3" />
+                    <li>
+                      <div className="dropdown-item">
+                        <div className="modeswitch m-0" id="darkModeSwitch">
+                          <div className="switch" />
+                        </div>
                       </div>
-                    </div>
-                    <hr />
-                  </li>
-                  {/* Links */}
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      <i className="bi bi-person fa-fw me-2" />
-                      Edit Profile
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      <i className="bi bi-gear fa-fw me-2" />
-                      Account Settings
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      <i className="bi bi-info-circle fa-fw me-2" />
-                      Help
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      <i className="bi bi-power fa-fw me-2" />
-                      Sign Out
-                    </a>
-                  </li>
-                  <li className="dropdown-divider mb-3" />
-                  <li>
-                    <div className="dropdown-item">
-                      <div className="modeswitch m-0" id="darkModeSwitch">
-                        <div className="switch" />
-                      </div>
-                    </div>
-                  </li>
-                </ul>
+                    </li>
+                  </ul>
                 </li>
               </ul>
-              {/* Search dropdown START */}
+
               <div className="nav my-3 my-xl-0 px-4 px-lg-1 flex-nowrap align-items-center">
                 <div className="nav-item w-100">
                   <form className="position-relative">
@@ -185,99 +271,9 @@ export default function Home() {
                   </form>
                 </div>
               </div>
-              {/* Search dropdown END */}
-            </div>
-            {/* Main navbar END */}
-            {/* Nav right START */}
-            <div className="nav flex-nowrap align-items-center">              
-              {/* Profile dropdown START */}
-              <div className="nav-item ms-2 ms-md-3 dropdown">
-                {/* Avatar */}
-                <a
-                  className="avatar avatar-sm p-0 show"
-                  href="#"
-                  id="profileDropdown"
-                  role="button"
-                  data-bs-auto-close="outside"
-                  data-bs-display="static"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="true"
-                >
-                  <img
-                    className="avatar-img rounded-circle"
-                    src="assets/images/avatar/03.jpg"
-                    alt="avatar"
-                  />
-                </a>
-                {/* Profile dropdown START */}
-                <ul
-                  className="dropdown-menu dropdown-animation dropdown-menu-end shadow pt-3 show"
-                  aria-labelledby="profileDropdown"
-                  data-bs-popper="static"
-                >
-                  {/* Profile info */}
-                  <li className="px-3">
-                    <div className="d-flex align-items-center">
-                      {/* Avatar */}
-                      <div className="avatar me-3">
-                        <img
-                          className="avatar-img rounded-circle shadow"
-                          src="assets/images/avatar/03.jpg"
-                          alt="avatar"
-                        />
-                      </div>
-                      <div>
-                        <a className="h6 mt-2 mt-sm-0" href="#">
-                          {" "}
-                          Louis Ferguson
-                        </a>
-                        <p className="small m-0">example@gmail.com</p>
-                      </div>
-                    </div>
-                    <hr />
-                  </li>
-                  {/* Links */}
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      <i className="bi bi-person fa-fw me-2" />
-                      Edit Profile
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      <i className="bi bi-gear fa-fw me-2" />
-                      Account Settings
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      <i className="bi bi-info-circle fa-fw me-2" />
-                      Help
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      <i className="bi bi-power fa-fw me-2" />
-                      Sign Out
-                    </a>
-                  </li>
-                  <li className="dropdown-divider mb-3" />
-                  <li>
-                    <div className="dropdown-item">
-                      <div className="modeswitch m-0" id="darkModeSwitch">
-                        <div className="switch" />
-                      </div>
-                    </div>
-                  </li>
-                </ul>
-                {/* Profile dropdown END */}
-              </div>
-              {/* Profile dropdown END */}
-              {/* Nav right END */}
             </div>
           </div>
         </nav>
-        {/* Logo Nav END */}
       </header>
     </>
   );
